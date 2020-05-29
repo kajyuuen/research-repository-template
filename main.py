df67700e992be0f37fd073489115cd9500e0cff9
Koga Kobayashi <kajyuuen@gmail.com>
2020-05-30 01:52:48 +0900

    Use WandbLogger

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from omegaconf import DictConfig
 
 import pytorch_lightning as pl
 from pytorch_lightning.logging import TensorBoardLogger
+from pytorch_lightning.loggers import WandbLogger
 
 import src.models
 
 @hydra.main(config_path="./configs/config.yaml")
 def main(cfg: DictConfig) -> None:
-    logger = TensorBoardLogger(utils.to_absolute_path("tb_logs"), name = cfg.name)
+    # logger = TensorBoardLogger(utils.to_absolute_path("tb_logs"), name = cfg.name)
+    logger = WandbLogger(name = cfg.wandb.name,
+                         project = cfg.name,
+                         offline = cfg.wandb.offline)
+    logger.log_hyperparams(cfg)
+
     trainer = pl.Trainer(logger)
     model = BaseModel(cfg)
     trainer.fit(model)
